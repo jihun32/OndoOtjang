381556ee366bce2bd68f767dde373f54b6a91a8e
jihun <jihun32@naver.com>
2025-09-07 21:46:07 +0900

    chore: Resource 폴더 계층 변경, DesignExample AppTarget 추가

---
+++ NalssiOtjang/Shared/Example/DesignSystemExample/View/ColorSystemView.swift
@@ -0,0 +1,18 @@
+//
+//  ColorTestView.swift
+//  NalssiOtjang
+//
+//  Created by home on 9/7/25.
+//
+
+import SwiftUI
+
+struct ColorTestView: View {
+    var body: some View {
+        Text("asdf")
+    }
+}
+
+#Preview {
+    ColorTestView()
+}
---
+++ NalssiOtjang/Shared/Example/DesignSystemExample/View/DesignSystemExampleView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignSystemExample
+//
+//  Created by home on 9/7/25.
+//
+
+import SwiftUI
+import SharedDesignSystem
+
+struct DesignSystemExampleView: View {
+    var body: some View {
+        NavigationStack {
+            
+        }
+    }
+}
+
+#Preview {
+    DesignSystemExampleView()
+}
