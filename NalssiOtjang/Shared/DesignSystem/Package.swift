dfd717be3fa5999af3e1717c1d9876f2a6f18b3f
jihun <jihun32@naver.com>
2025-09-07 21:47:21 +0900

    chore: DesignSystem -> SharedDesignSystem으로 import할 수 있게 변경

--- NalssiOtjang/Shared/DesignSystem/Package.swift
+++ NalssiOtjang/Shared/DesignSystem/Package.swift
@@ -4,20 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "DesignSystem",
+    name: "SharedDesignSystem",
+    platforms: [
+        .iOS(.v26)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "DesignSystem",
-            targets: ["DesignSystem"]
+            name: "SharedDesignSystem",
+            targets: ["SharedDesignSystem"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DesignSystem"
-        ),
-
+            name: "SharedDesignSystem",
+            resources: [
+                .process("Resources")
+            ]
+        )
     ]
 )
